784ca628c6a6f3e7ac96b39c8308b5536c125372
velievco.gihub.io <velievco@gmail.com>
2026-03-02 00:00:20 +0300

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
 // Supabase → Settings → API → вставь свои значения:
-const SUPABASE_URL = 'ВСТАВЬ_PROJECT_URL';
-const SUPABASE_ANON_KEY = 'ВСТАВЬ_ANON_KEY_API';
+const SUPABASE_URL = 'https://ewhwpzejwtnwelieuomz.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV3aHdwemVqd3Rud2VsaWV1b216Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzNzk4MzIsImV4cCI6MjA4Nzk1NTgzMn0.DDGZJHx8ANS-8xz92fD0WlYjIbhFIJvvhhHiZGODW9o';
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
