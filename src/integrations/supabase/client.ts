e5ed859463f6ee91b15c96ea32f944531fb136b3
velievco.gihub.io <velievco@gmail.com>
2026-03-06 23:30:09 +0300

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const SUPABASE_URL = 'https://ntkyigwpbnsxkpkjvdpj.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_Aikzr4-zCblvY8PBAiY4Lw_JtkXryde';
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
